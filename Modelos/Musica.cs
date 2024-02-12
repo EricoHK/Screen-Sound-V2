f57b63d203f240c0bc9bd9688023c4f78b796ed5
EricoHK <ericohq12@gmail.com>
2024-02-12 11:41:17 -0300

    Adicionar arquivos de projeto.

---
+++ Modelos/Musica.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ScreenSound_04.Modelos;
+
+internal class Musica
+{
+    // Definindo propriedades que correspondem aos campos do JSON
+
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+    [JsonPropertyName("year")]
+    public string? AnoString { get; set; }
+    [JsonPropertyName("key")]
+    public int KeyInt { get; set; }
+
+    public string Key
+    {
+        get
+        {
+            string KeyString = "";
+            switch (KeyInt)
+            {
+                case 0: KeyString = "C";
+                    break;
+                case 1: KeyString = "C#";
+                    break;
+                case 2: KeyString = "D";
+                    break;
+                case 3: KeyString = "D#";
+                    break;
+                case 4: KeyString = "E";
+                    break;
+                case 5: KeyString = "F";
+                    break;
+            }
+
+            return KeyString;
+        }
+    }
+    
+    public int Ano
+    {
+        get
+        {
+            return int.Parse(AnoString!);
+        }
+    }
+
+    public void ExibirDetalhesDaMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Música: {Nome}");
+        Console.WriteLine($"Duração Em Segundos: {Duracao / 1000}");
+        Console.WriteLine($"Gênero Musical: {Genero}");
+        Console.WriteLine($"Tonalidade: {Key}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using ScreenSound_04.Filtros;
+using ScreenSound_04.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new())
+{
+    // Esse HttpClient vai ser usado só aqui.
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+
+        // Desserialização
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!; // "!" = Não pode ser nulo.
+        musicas[0].ExibirDetalhesDaMusica();
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
+        //LinqFilter.FiltarMusicasPorArtista(musicas, "U2");
+        //LinqFilter.FiltrarMusicasPorAno(musicas, 2014);
+
+        var musicasPreferidasDoErico = new MusicasPreferidas("Erico");
+        musicasPreferidasDoErico.AdicionarMusicasFavoritas(musicas[1]);
+        musicasPreferidasDoErico.AdicionarMusicasFavoritas(musicas[2]);
+        musicasPreferidasDoErico.AdicionarMusicasFavoritas(musicas[3]);
+        musicasPreferidasDoErico.AdicionarMusicasFavoritas(musicas[4]);
+        musicasPreferidasDoErico.AdicionarMusicasFavoritas(musicas[5]);
+
+        musicasPreferidasDoErico.ExibirMusicasFavoritas();
+
+        var musicasPreferidasDoAlfredo = new MusicasPreferidas("Alfredo");
+        musicasPreferidasDoAlfredo.AdicionarMusicasFavoritas(musicas[20]);
+        musicasPreferidasDoAlfredo.AdicionarMusicasFavoritas(musicas[19]);
+        musicasPreferidasDoAlfredo.AdicionarMusicasFavoritas(musicas[18]);
+        musicasPreferidasDoAlfredo.AdicionarMusicasFavoritas(musicas[17]);
+        musicasPreferidasDoAlfredo.AdicionarMusicasFavoritas(musicas[16]);
+
+        musicasPreferidasDoAlfredo.ExibirMusicasFavoritas();
+        musicasPreferidasDoErico.GerarArquivoJson();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
